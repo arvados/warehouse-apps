9e0b87bc8ec8f446ba8fd1a777b93d094c7bd047
Tom Clegg <tom@clinicalfuture.com>
2007-05-07 19:02:44 +0000

    Add www/manifest.php
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@2 1b2bb62c-e52f-0410-8f12-f050cf38ac25

---
+++ www/manifest.php
@@ -0,0 +1,17 @@
+<?php
+
+$keyprefix = $_REQUEST['keyprefix'];
+$tmpfile = tempnam("/tmp", "mogextract.");
+exec ("mogtool listkey ".escapeshellarg($keyprefix), $keylist)
+foreach ($keylist as $key)
+{
+  exec ("mogtool extract --overwrite ".escapeshellarg($key)." ".escapeshellarg($tmpfile)." || rm -f ".escapeshellarg($tmpfile));
+  if (file_exists($tmpfile))
+    {
+      $md5 = md5(file_get_contents($tmpfile));
+      unlink($tmpfile);
+      echo "$md5 $key";
+    }
+}
+// arch-tag: 5175c8f5-fccd-11db-9207-0015f2b17887
+?>
