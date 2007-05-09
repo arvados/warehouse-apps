314129f65d0e03121f60c108579ea2400d63f4e5
Tom Clegg <tom@clinicalfuture.com>
2007-05-09 20:31:58 +0000

    show total #bytes in domain on cluster.php
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@15 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- www/cluster.php
+++ www/cluster.php
@@ -3,10 +3,12 @@
 require_once 'inc-mogilefs.php';
 $domain = isset($_REQUEST['domain'])? $_REQUEST['domain'] : "default";
 $grok = array();
+$lengthtotal = 0;
 
 function grok ($fid=undef, $length=undef, $dkey=undef)
 {
   global $grok;
+  global $lengthtotal;
   if ($fid !== undef)
     {
       $dkey = explode("/", ereg_replace("^/*","",$dkey));
@@ -24,19 +26,25 @@ function grok ($fid=undef, $length=undef, $dkey=undef)
 	}
       $grok[$dataset][$subdir][length] += $length;
       $grok[$dataset][$subdir][count] ++;
+      $lengthtotal += $length;
     }
   else
     {
-      echo "<table>\n";
-      echo "<tr><th align=left>dataset</th><th align=left>base</th><th align=left>files</th><th align=left>bytes</th></tr>\n";
+      echo "<table cellspacing=0 cellpadding=0 border=0>\n";
+      echo "<tr><th align=left>dataset</th><th align=left>cycle</th><th align=right>&nbsp;files&nbsp;</th><th align=right>bytes</th></tr>\n";
       foreach ($grok as $dataset => $d)
 	{
 	  echo "<tr><td>$dataset</td></tr>\n";
 	  foreach ($d as $subdir => $s)
 	    {
-	      echo "<tr><td></td><td>$subdir</td><td>$s[count]</td><td>$s[length]</td></tr>\n";
+	      echo "<tr><td></td><td>$subdir</td><td align=right>&nbsp;$s[count]&nbsp;</td><td align=right>$s[length]</td></tr>\n";
 	    }
 	}
+      while (ereg("[0-9][0-9][0-9][0-9]", $lengthtotal))
+	{
+	  $lengthtotal = ereg_replace ("([0-9]+)([0-9][0-9][0-9])", "\\1,\\2", $lengthtotal);
+	}
+      echo "<th align=left>total</th><td></td><td></td><td align=right>$lengthtotal</td></tr>\n";
       echo "</table>\n";
     }
 }
