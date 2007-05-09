3a2c9979488884511099fc3dea06110d46f2cbff
Tom Clegg <tom@clinicalfuture.com>
2007-05-09 05:47:14 +0000

    add --batch=N knob to dssync
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@12 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- www/upload.php
+++ www/upload.php
@@ -4,16 +4,21 @@ require_once 'inc-mogilefs.php';
 $domain = $_REQUEST['domain'];
 $class = $_REQUEST['class'];
 
-$key = $_POST['key'];
-$tmpfile = $_FILES['upload']['tmp_name'];
-exec ("./inject"
-      ." ".escapeshellarg($mogilefs_trackers)
-      ." ".escapeshellarg($domain)
-      ." ".escapeshellarg($class)
-      ." ".escapeshellarg($key)
-      ." ".escapeshellarg($tmpfile));
-include 'checkmd5.php';
-unlink($tmpfile);
+for ($i=""; isset($_FILES["upload$i"]); $i=0+$i+1)
+{
+  $key = $_POST["key$i"];
+  $tmpfile = $_FILES["upload$i"]['tmp_name'];
+  exec ("echo $key $tmpfile >&2");
+  exec ("./inject"
+	." ".escapeshellarg($mogilefs_trackers)
+	." ".escapeshellarg($domain)
+	." ".escapeshellarg($class)
+	." ".escapeshellarg($key)
+	." ".escapeshellarg($tmpfile));
+  include 'checkmd5.php';
+  echo mogilefs_getmd5(mogilefs_getfid($key, $domain)) . "\n";
+  unlink ($tmpfile);
+}
 
 // arch-tag: 74aa41a2-f9a2-11db-9207-0015f2b17887
 ?>
