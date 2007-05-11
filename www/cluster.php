5c84afdd6a6227b4f5a573b8198e7bce29966d9d
Tom Clegg <tom@clinicalfuture.com>
2007-05-11 18:44:36 +0000

    show total files & bytes for each dataset
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@31 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- www/cluster.php
+++ www/cluster.php
@@ -5,15 +5,25 @@ $domain = isset($_REQUEST['domain'])? $_REQUEST['domain'] : "default";
 $grok = array();
 $lengthtotal = 0;
 
+function insertcommas ($n)
+{
+  while (ereg("[0-9][0-9][0-9][0-9]", $n))
+    {
+      $n = ereg_replace ("([0-9]+)([0-9][0-9][0-9])", "\\1,\\2", $n);
+    }
+  return $n;
+}
+
 function grok ($fid=undef, $length=undef, $dmid=undef, $dkey=undef)
 {
   global $grok;
   global $lengthtotal;
+  global $namespace;
   if ($fid !== undef)
     {
       $dkey = explode("/", ereg_replace("^/*","",$dkey));
       $subdir = $dkey[count($dkey)-2];
-      $dataset = $dkey[0];
+      $dataset = "[".$namespace[$dmid]."]".$dkey[0];
       if (!isset($grok[$dataset]))
 	{
 	  $grok[$dataset] = array();
@@ -30,22 +40,35 @@ function grok ($fid=undef, $length=undef, $dmid=undef, $dkey=undef)
     }
   else
     {
+      echo "<div style=\"font-size: 9pt;\">\n";
       echo "<table cellspacing=0 cellpadding=0 border=0>\n";
       echo "<tr><th align=left>dataset</th><th align=left>cycle</th><th align=right>&nbsp;files&nbsp;</th><th align=right>bytes</th></tr>\n";
       foreach ($grok as $dataset => $d)
 	{
+	  $stfiles = 0;
+	  $stbytes = 0;
 	  echo "<tr><td>$dataset</td></tr>\n";
 	  foreach ($d as $subdir => $s)
 	    {
-	      echo "<tr><td></td><td>$subdir</td><td align=right>&nbsp;$s[count]&nbsp;</td><td align=right>$s[length]</td></tr>\n";
+	      echo "<tr><td></td><td>$subdir</td><td align=right>&nbsp;"
+		.insertcommas($s[count])
+		."&nbsp;</td><td align=right>"
+		.insertcommas($s[length])
+		."</td></tr>\n";
+	      $stfiles += $s[count];
+	      $stbytes += $s[length];
 	    }
+	  echo "<tr><td></td><td></td><td align=right><i>&nbsp;"
+	    .insertcommas($stfiles)
+	    ."&nbsp;</i></td><td align=right><i>"
+	    .insertcommas($stbytes)
+	    ."</i></td></tr>\n";
 	}
-      while (ereg("[0-9][0-9][0-9][0-9]", $lengthtotal))
-	{
-	  $lengthtotal = ereg_replace ("([0-9]+)([0-9][0-9][0-9])", "\\1,\\2", $lengthtotal);
-	}
-      echo "<th align=left>total</th><td></td><td></td><td align=right>$lengthtotal</td></tr>\n";
+      echo "<th align=left>total</th><td></td><td></td><td align=right><i><b>"
+	.insertcommas($lengthtotal)
+	."</b></i></td></tr>\n";
       echo "</table>\n";
+      echO "</div>\n";
     }
 }
 
