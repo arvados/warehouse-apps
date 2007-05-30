374082999ce4ee838f352f31e7d6fc2b5237ccbd
Tom Clegg <tom@clinicalfuture.com>
2007-05-30 22:36:54 +0000

    minor optimizations in manifest.php
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@86 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- www/manifest.php
+++ www/manifest.php
@@ -6,36 +6,38 @@ set_time_limit($_REQUEST['recompute_all_md5'] ? 86400 : 120);
 require_once 'inc-mogilefs.php';
 $domain = isset($_REQUEST['domain'])? $_REQUEST['domain'] : "default";
 
+list ($dmid) = mysql_fetch_row (mysql_query ("select dmid from domain where namespace='$domain'"));
+
 $keyprefix = $_REQUEST['keyprefix'];
 $q = mysql_query("select
      file.fid,
      file.length,
      file.dkey,
      md5,
-     concat('http://',hostip,':',http_port,'/dev',device.devid)
+     hostip, http_port, device.devid
      from file
      left join file_on on file_on.fid=file.fid
      left join device on device.devid=file_on.devid
      left join host on host.hostid=device.hostid
-     left join domain on domain.dmid=file.dmid
      left outer join md5 on md5.fid=file.fid
-     where dkey like '$keyprefix%'
-     and domain.namespace='$domain'
-     order by dkey, rand()");
+     where dmid='$dmid'
+     and dkey like '$keyprefix%'");
 echo mysql_error();
 $lastfid = -1;
 while($row = mysql_fetch_row($q))
 {
-  list ($fid, $length, $dkey, $md5, $devpath) = $row;
+  list ($fid, $length, $dkey, $md5, $hostip, $http_port, $devid) = $row;
   if ($fid == $lastfid) continue;
   $lastfid = $fid;
-  $fid = sprintf("%010d", $fid);
+  if (strlen ($fid) < 10)
+    $fid = sprintf("%010d", $fid);
   if ("$md5" == "" && $_REQUEST['quick'])
     {
       $md5 = "oooooooooooooooooooooooooooooooo";
     }
   if ("$md5" == "" || $_REQUEST['recompute_all_md5'])
     {
+      $devpath = "http://" . $hostip . ":" . $http_port . "/dev" . $devid;
       $url = $devpath."/"
 	.substr($fid,0,1)."/"
 	.substr($fid,1,3)."/"
