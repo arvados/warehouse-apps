67e5b40a935d11fa8e821df26d932dfa32cbddbe
Tom Clegg <tom@clinicalfuture.com>
2007-06-11 22:01:22 +0000

    fix wrong path to onejob.sh
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@104 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- www/jobsubmit.php
+++ www/jobsubmit.php
@@ -56,7 +56,7 @@ for ($f=1; $f<=$nframes; $f++)
   $dkey_stdout="/$rid/frame/$fid";
   $dkey_stderr="/$rid/frame/$fid.stderr";
   $jobname = escapeshellarg("$rid:$fid:$dsid");
-  $cmd = "srun --job-name=$jobname $depends --batch --chdir=$revisiondir --output=/tmp/stdout --error=/tmp/stderr ./onejob.sh";
+  $cmd = "srun --job-name=$jobname $depends --batch --chdir=$revisiondir --output=/tmp/stdout --error=/tmp/stderr onejob.sh";
   mysql_query ("insert into job set
  jid=null,
  rid='$rid',
