69361fae66229ea77dc9b7f7ee819c361f6cf52e
Tom Clegg <tom@clinicalfuture.com>
2007-06-18 23:03:48 +0000

    get.php - improve filetype logic, tell browser what filename to save as
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@116 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- www/get.php
+++ www/get.php
@@ -6,13 +6,22 @@ require_once 'functions.php';
 putenv("MOGILEFS_DOMAIN=".$_REQUEST[domain]);
 putenv("MOGILEFS_TRACKERS=".join(",", $mogilefs_trackers));
 
+$filename = ereg_replace ("/", "-", ereg_replace("^/", "", $_REQUEST[dkey]));
+header("Content-Disposition: attachment; filename=\"$filename\"");
 if ($_REQUEST[domain] == 'reports')
 {
   header("Content-type: text/plain");
 }
 else if ($_REQUEST[domain] == 'images')
 {
-  header("Content-type: image/tiff");
+  if (ereg ("/(positions|cycles)$", $_REQUEST[dkey]))
+    {
+      header ("Content-type: text/plain");
+    }
+  else
+    {
+      header("Content-type: image/tiff");
+    }
 }
 else
 {
@@ -20,6 +29,7 @@ else
 }
 
 $safekey = escapeshellarg($_REQUEST[dkey]);
+
 echo_file_get_contents(trim(`perl moggetpaths.pl $safekey`));
 
 ?>
