2da8effc5483992f73bcf89535cf0e73f8ab0fc8
Tom Clegg <tom@clinicalfuture.com>
2007-06-19 05:26:28 +0000

    display aggregate MB/s on stdout, omit "skip" messages if no -v switch
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@121 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- storage/dscopy.pl
+++ storage/dscopy.pl
@@ -4,11 +4,12 @@ use strict;
 use MogileFS::Client;
 use Digest::MD5 'md5_hex';
 use DBI;
+use Time::HiRes qw(gettimeofday tv_interval);
 
 do '/etc/polony-tools/config.pl';
 
 my %opts = qw (quick 0);
-while (@ARGV && $ARGV[0] =~ /^--(.*?)(=(.*))?$/)
+while (@ARGV && $ARGV[0] =~ /^--?([^-].*?)(=(.*))?$/)
 {
     if (defined ($2))
     {
@@ -43,13 +44,16 @@ my $copyto_dbh = DBI->connect($main::copyto_mogilefs_dsn,
 
 my $sth = $dbh->prepare("select dkey, md5 from file left join md5 on md5.fid=file.fid where dkey like ? order by dkey");
 
-my $copyto_sth = $copyto_dbh->prepare("select dkey, md5 from file left join md5 on md5.fid=file.fid left join file_on on md5.fid=file_on.fid where dkey like ? and file_on.fid is not null order by dkey");
+my $copyto_sth = $copyto_dbh->prepare("select dkey, md5 from file left join md5 on md5.fid=file.fid left join file_on on file.fid=file_on.fid where dkey like ? and file_on.fid is not null order by dkey");
 
 $sth->execute ($keyprefix . "%") or die;
 $copyto_sth->execute ($keyprefix . "%") or die;
 
 my @row = $sth->fetchrow_array;
 my @copyto_row = $copyto_sth->fetchrow_array;
+
+my $txbytes = 0;
+my $t0 = [gettimeofday];
 
 while (@row)
 {
@@ -60,7 +64,8 @@ while (@row)
 	    || !defined($row[1])
 	    || !defined($copyto_row[1])))
     {
-	printf ("skip    %-32s %s\n", $row[1], $row[0]);
+	printf ("skip    %s\n", $row[0])
+	    if $opts{v};
     }
     # else, fetch from $mogc and inject in $copyto_mogc
     else
@@ -68,9 +73,9 @@ while (@row)
 	if (@copyto_row
 	    && $row[0] eq $copyto_row[0])
 	{
-	    printf ("XXX md5 %-32s %s\n", $copyto_row[1], $copyto_row[0]);
+	    printf ("md5!!!! %-32s %s\n", $copyto_row[1], $copyto_row[0]);
 	}
-	printf ("copy    %-32s %s\n", $row[1], $row[0]);
+	printf ("copy    %-50s ", $row[0]);
 
 	my ($dkey, $md5) = @row;
 	my $content = $mogc->get_file_data ($dkey);
@@ -78,11 +83,17 @@ while (@row)
 	    ($dkey,
 	     $main::copyto_mogilefs_default_class,
 	     $content);
-	if ($ok && !$opts{quick})
+	if ($ok)
 	{
-	    my $md5_sth = $copyto_dbh->prepare ("insert delayed into md5 (fid, md5) select fid, ? from file left join domain on domain.dmid=file.dmid where dkey=? and domain.namespace=?");
-	    $md5_sth->execute ($md5, $dkey, $main::copyto_mogilefs_default_domain);
+	    $txbytes += length $$content;
+	    if (!$opts{quick})
+	    {
+		my $md5_sth = $copyto_dbh->prepare ("insert delayed into md5 (fid, md5) select fid, ? from file left join domain on domain.dmid=file.dmid where dkey=? and domain.namespace=?");
+		$md5_sth->execute ($md5, $dkey, $main::copyto_mogilefs_default_domain);
+	    }
 	}
+	my $speed = $txbytes / 1048576 / tv_interval($t0);
+	printf ("%0.2f MB/s\n", $speed);
 	if (!$ok)
 	{
 	    printf ("FAIL    %-32s %s\n", $row[1], $row[0]);
