82d6d170ef7ac4f94b46cb129369a7321ec12981
Tom Clegg <tom@clinicalfuture.com>
2007-06-30 02:00:53 +0000

    check #images read = #images expected according to specified DIRORDER
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@132 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- align-call/find_objects-register_raw_pipe.pl
+++ align-call/find_objects-register_raw_pipe.pl
@@ -2,41 +2,43 @@
 
 #adding a comment here -- AWZ
 
-use strict; 
+use strict;
 
-my $PIXELS = 1000 * 1000; 
-my $RECORDSIZE = 2 * $PIXELS; 
+my $PIXELS = 1000 * 1000;
+my $RECORDSIZE = 2 * $PIXELS;
 
-my @raw; 
-my $num_raw = 0;  
+my $num_raw_expected = $ENV{DIRORDER} =~ tr/ / /;
+$num_raw_expected = ($num_raw_expected + 1) * 4; # flor images
+$num_raw_expected++;		# bright field
+if (exists($ENV{HYBRIDDIRORDER})
+    &&
+    $ENV{HYBRIDDIRORDER} =~ /\S/)
+{
+    $num_raw_expected += $ENV{HYBRIDDIRORDER} =~ tr/ / /;
+    $num_raw_expected ++;
+}
+
+my @raw;
+my $num_raw = 0; 
 
 until ( eof(STDIN) ) {
     read(STDIN, $raw[$num_raw], $RECORDSIZE) == $RECORDSIZE
         or die "input must be multiple of $RECORDSIZE bytes\n";
-    $num_raw++; 
+    $num_raw++;
 }
-warn "Read $num_raw raw images\n"; 
+warn "Read $num_raw raw images\n";
 
-if ($num_raw < 1 ) {
-  die "not enough data for brightfield\n";
+if ($num_raw != $num_raw_expected) {
+    die "Expected $num_raw_expected images, read $num_raw";
 }
-if (($num_raw-1) % 4 != 0) {
-  die "florescence images should be provided in multiples of four.\n";  
-}
-open SPOOLER, "| find-objects 1000 1000 $ENV{OBJECTTHRESHOLD} 1"; 
-
-print SPOOLER $raw[0]; 
-
-close SPOOLER; 
+open SPOOLER, "| find-objects 1000 1000 $ENV{OBJECTTHRESHOLD} 1";
+print SPOOLER $raw[0];
+close SPOOLER;
 
 for (my $i = 1; $i < $num_raw; $i++) {
-
   open SPOOLER, "| register-raw-translate 1000 1000 $ENV{FOCUSPIXELS} $ENV{OBJECTTHRESHOLD} 1 $ENV{ALIGNWINDOW}";
-
-  print SPOOLER $raw[0].$raw[$i]; 
-  
-  close SPOOLER; 
-
+  print SPOOLER $raw[0].$raw[$i];
+  close SPOOLER;
 }
 
 # arch-tag: Tom Clegg Fri Mar 16 20:44:33 PDT 2007 (align-read/find_objects-register_raw_pipe.pl)
