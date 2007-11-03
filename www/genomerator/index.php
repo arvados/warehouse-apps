3289657be991917d24fb8780ccaed38bed2f813f
Tom Clegg <tom@clinicalfuture.com>
2007-11-03 22:04:45 +0000

    genomerator: show pmage_wt_403_8 if vhost is pmage-wt-403-8.freelogy.org
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@532 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- www/genomerator/index.php
+++ www/genomerator/index.php
@@ -1,6 +1,16 @@
 <?php
 
-$dsid = ereg_replace ("[^-_a-zA-Z0-9]", "", $_REQUEST["dsid"]);
+if (isset ($_SERVER["DSID"])) {
+  $dsid = $_SERVER["DSID"];
+}
+else if (isset ($_ENV["DSID"]))
+{
+  $dsid = $_SERVER["DSID"];
+}
+else
+{
+  $dsid = ereg_replace ("[^-_a-zA-Z0-9]", "", $_REQUEST["dsid"]);
+}
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
