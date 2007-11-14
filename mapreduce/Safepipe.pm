ae18d396e1646c9a90c6b830b3e63265eab63688
Tom Clegg <tom@clinicalfuture.com>
2007-11-14 09:31:32 +0000

    Add Safepipe.pm, for detecting failure of mid-pipeline commands
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@561 1b2bb62c-e52f-0410-8f12-f050cf38ac25

---
+++ mapreduce/Safepipe.pm
@@ -0,0 +1,58 @@
+package Safepipe;
+
+sub readfrom
+{
+    open (shift @_, "-|") && return;
+
+    my @caller = caller;
+    my @children;
+    my $haveoutput = 0;
+    my $n = 0;
+    my $lastreadpipe;
+    while (@_)
+    {
+	$n++;
+	my $command = shift @_;
+	pipe ("read$n", "write$n") or die "@caller: $!";
+	my $child = fork();
+	die "@caller: $!" if !defined $child;
+	if ($child == 0)
+	{
+	    close "read$n" or die "$!";
+	    if (defined $lastreadpipe)
+	    {
+		open STDIN, "<&$lastreadpipe" or die "$!";
+	    }
+	    else
+	    {
+		close STDIN;
+	    }
+	    open STDOUT, ">&write$n" or die "$!";
+	    exec $command;
+	    die "@caller: $!";
+	}
+	close "write$n" or die "$!";
+	$lastreadpipe = "read$n";
+
+	push @children, $child;
+    }
+    if (defined $lastreadpipe)
+    {
+	open STDIN, "<&$lastreadpipe" or die "$!";
+    }
+    my $buf;
+    while (read STDIN, $buf, 1048576)
+    {
+	print STDOUT $buf;
+    }
+    while (@children)
+    {
+	my $pid = wait;
+	die "@caller: $pid exited $?" if $? != 0;
+	pop @children;
+    }
+    close STDIN or die "@caller: $!";
+    exit 0;
+}
+
+1;
