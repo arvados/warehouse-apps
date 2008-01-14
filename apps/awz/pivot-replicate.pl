8ada292373c0163671a4066c933db85e649e86a9
Alexander Wait Zaranek <awaitz@post.harvard.edu>
2008-01-14 18:50:46 +0000

    add options to pivot-replicate
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@993 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- apps/awz/pivot-replicate.pl
+++ apps/awz/pivot-replicate.pl
@@ -6,45 +6,58 @@ use Warehouse;
 use Digest::MD5;
 use HTTP::Request::Common;
 use LWP::UserAgent;
+use Getopt::Std;
+
+my %args;
+getopts ("rn:m:c:", \%args);
 
 my $whc = new Warehouse ();
 
-my $manifestkey = "4b0e254eb53972c6ad053365fc8004bf";
+my $manifestkey = $args{'m'}; 
+
+my $new_manifestname = $args{'n'}; 
+
+my $copies = $args{'c'}; 
+
+print "Expanding $manifestkey into $new_manifestname with $copies copies\n"; 
+
 my @manifesthash= split (",", $manifestkey); 
 
-#read first manifest block 
+#read first manifest block but there could be others... 
 my $manifest = $whc->fetch_block (shift @manifesthash)
     or die "fetch_block failed";
 
-#split manifest into separate lines (what perlism am I missing here?) 
-my @streams = split /\n/, $manifest;
-
-my @blocks;
-my @files; 
-my $bytes_in_files = 0;
-my $bytes_in_blocks = 0; 
-
-foreach my $stream (@streams) { 
-  my @tokens = split ' ',$stream;
-  foreach my $x (@tokens) { 	
-    if ($x =~ m/^[0-9a-f]{32}\+([0-9]+)/) {
-      push @blocks, $x; 
-      $bytes_in_blocks+=$1;
-    } 
-    elsif ($x =~ m/^[0-9]+:([0-9]+):(.+)/) {
-      push @files, "$bytes_in_files:$1:$2";
-      $bytes_in_files+=$1;
-    }
-  }
-}
-
-if ($bytes_in_blocks ne $bytes_in_files) {
-  die "error!\n";
-}
-
 my $new_manifest = ""; 
-  
-foreach my $rep (1 .. 20) {
+
+foreach my $rep (0 .. ($copies-1)) {
+
+  my @streams = split /\n/, $manifest;
+  my @blocks;
+  my @files; 
+  my $bytes_in_files = 0;
+  my $bytes_in_blocks = 0; 
+
+  while (my $length = scalar(@streams)) { 
+    my $i = 0; 
+    if ($args{'r'}) { 
+      $i = rand @streams;    
+    } 
+    my $stream = splice (@streams, $i, 1); 
+    my @tokens = split ' ',$stream;
+    foreach my $x (@tokens) { 	
+      if ($x =~ m/^[0-9a-f]{32}\+([0-9]+)/) {
+        push @blocks, $x; 
+        $bytes_in_blocks+=$1;
+      } 
+      elsif ($x =~ m/^[0-9]+:([0-9]+):(.+)/) {
+        push @files, "$bytes_in_files:$1:$2";
+        $bytes_in_files+=$1;
+      }
+    }
+  }	
+  if ($bytes_in_blocks ne $bytes_in_files) {
+    die "error!\n";
+  }
   $new_manifest .= "./$rep @blocks @files\n";
 }
    
@@ -54,11 +67,9 @@ my $new_manifest_key = $whc->write_finish;
 
 print "Manifest key is: $new_manifest_key\n";
 
-my $manifestname = "test"; 
-
-my $oldkey = $whc->fetch_manifest_key_by_name ($manifestname);
-$whc->store_manifest_by_name ($new_manifest_key, $oldkey, $manifestname);
-my $checkkey = $whc->fetch_manifest_key_by_name ($manifestname);
+my $oldkey = $whc->fetch_manifest_key_by_name ($new_manifestname);
+$whc->store_manifest_by_name ($new_manifest_key, $oldkey, $new_manifestname);
+my $checkkey = $whc->fetch_manifest_key_by_name ($new_manifestname);
     
-print "fetch says: $manifestname => $checkkey\n";
+print "fetch says: $new_manifestname => $checkkey\n";
 
