899f5ffd15a56a20202a40c650e5c7ef5de85487
Alexander Wait Zaranek <awaitz@post.harvard.edu>
2008-04-14 14:19:42 +0000

    always get all jobs in the warehouse so far
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@1919 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- apps/awz/meta2manifest.pl
+++ apps/awz/meta2manifest.pl
@@ -12,7 +12,7 @@ getopts "m:", \%args);
 my $whc;
 $whc = new Warehouse ($args{'m'} ? (warehouse_name => $args{'m'}) : ());
 
-my $joblist = $whc->job_list (%opt);
+my $joblist = $whc->job_list ();
 if ($joblist) {
     foreach my $j (@$joblist) {
 	print join (", ", map { $_ . "=" . $j->{$_} } sort keys %$j) . "\n";
@@ -22,21 +22,3 @@ else {
     warn ($whc->errstr . "\n"); 
 }
 
-
-sub job_new
-{
-    my @knobs = split (/\n/, $opt{'knobs'});
-    foreach (sort keys %opt)
-    {
-	if (!/[a-z]/)		# treat uppercase wh opts as job knobs
-	{
-	    push @knobs, $_."=".$opt{$_};
-	    delete $opt{$_};
-	}
-    }
-    $opt{'knobs'} = join ("\n", @knobs);
-
-    my $jobid = $whc->job_new (%opt);
-    if ($jobid) { print ($jobid . "\n"); }
-    else { warn ($whc->errstr . "\n"); }
-}
