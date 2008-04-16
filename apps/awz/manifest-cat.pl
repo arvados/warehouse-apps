8b0e1633e6c5f973fd156b1ff3007c9b0485f298
Alexander Wait Zaranek <awaitz@post.harvard.edu>
2008-04-16 16:50:24 +0000

    add manifest-cat.pl utility to dump contents of a manifest to stdout.
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@1935 1b2bb62c-e52f-0410-8f12-f050cf38ac25

---
+++ apps/awz/manifest-cat.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+# -*- mode: perl; perl-indent-level: 2; -*-
+
+use strict;
+use Warehouse;
+use Getopt::Std;
+
+my %args;
+getopts ("w:m:", \%args);
+
+my $whc = new Warehouse 
+    ($args{'w'} ? (warehouse_name => $args{'w'}) : ());
+
+my $manifestkey;
+my $manifestname = $args{'m'}; 
+
+if ($manifestname =~ m/[0-9a-f]{32}/) {
+  $manifestkey = $manifestname; 
+}
+else {
+  $manifestkey = $whc->fetch_manifest_key_by_name ($manifestname);
+}
+
+my $manifest = new Warehouse::Manifest (whc => $whc,
+					key => $manifestkey);
+
+$manifest->rewind; 
+
+while (my $instream = $manifest->subdir_next ) {
+  while (my ($pos, $size, $filename) = $instream->file_next) {
+    last if !defined $pos;
+    print STDERR "$filename\n"; 
+    while (my $inputline = $instream->read_until($pos+$size, "\n")) {
+      print $inputline;
+    }
+  }
+}
