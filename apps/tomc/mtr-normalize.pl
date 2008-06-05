9275130f93ea9c53c5b2b251e37cb7dd0d1f1929
Tom Clegg <tom@clinicalfuture.com>
2008-06-05 21:39:25 +0000

    erez-jobs: tweak erez-jobs->mtr-normalize api, skip R step if graph newer than mtr-count output
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@2051 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- apps/tomc/mtr-normalize.pl
+++ apps/tomc/mtr-normalize.pl
@@ -1,16 +1,19 @@
 #!/usr/bin/perl
 
+my $m = shift;
+
 while(<>)
 {
-    if (/^\d/)
+    if (/^(\d+)/)
     {
+	next if $1 != $m;
 	my @a = split /\t/;
 	my $sum = 0;
 	map { $sum += $a[$_] } (2..$#a);
 	map { $a[$_] = sprintf "%.2f", $a[$_] * 100 / $sum } (2..$#a);
 	print (join ("\t", @a), "\n");
     }
-    else
+    elsif (/^m\s/)
     {
 	print;
     }
