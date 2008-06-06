adba57d26eb3dc5139e3bd8b45b91dd7ce5eee81
Tom Clegg <tom@clinicalfuture.com>
2008-06-06 02:17:34 +0000

    erez-jobs: show megatri stats outputs, and create "examples" files for each {m,n} with proportional numbers of each xy
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@2053 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- apps/tomc/mtr-normalize.pl
+++ apps/tomc/mtr-normalize.pl
@@ -1,20 +1,23 @@
 #!/usr/bin/perl
 
 my $m = shift;
+$" = "\t";
 
 while(<>)
 {
-    if (/^(\d+)/)
+    chomp;
+    if (/^(\d+)\t[\d\t]*$/)
     {
 	next if $1 != $m;
 	my @a = split /\t/;
 	my $sum = 0;
-	map { $sum += $a[$_] } (2..$#a);
-	map { $a[$_] = sprintf "%.2f", $a[$_] * 100 / $sum } (2..$#a);
-	print (join ("\t", @a), "\n");
+	map { $sum += $a[$_] } (2..13);
+	map { $a[$_] = sprintf "%.2f", $a[$_] * 100 / $sum } (2..13);
+	print "@a\n";
     }
     elsif (/^m\s/)
     {
-	print;
+	my @a = split /\t/;
+	print "@a\n";
     }
 }
--- apps/tomc/mtr-sum.pl
+++ apps/tomc/mtr-sum.pl
@@ -5,11 +5,17 @@ use strict;
 use Warehouse;
 use Warehouse::Stream;
 use Warehouse::Manifest;
+use IO::File;
+
+my $manifestkey = shift @ARGV;
+my $examplesbase = shift @ARGV;
 
 my $whc = new Warehouse;
 my $m = new Warehouse::Manifest (whc => $whc,
-				 key => $ARGV[0]);
+				 key => $manifestkey);
+
 my %sum;
+my %summaryfields;
 while (my $s = $m->subdir_next)
 {
   $s->rewind;
@@ -23,15 +29,28 @@ while (my $s = $m->subdir_next)
       {
 	$sum{"$1,$2,$3"} += $4;
       }
+      elsif ($$dataref =~ /^\#summary: m=(\d+) n=(\d+) (.*)/)
+      {
+	my $m = $1;
+	my $n = $2;
+	my $s = $3;
+	while ($s =~ /\b(\S+)=(\d+)\b/g)
+	{
+	  $sum{"$m,$n,$1"} += $2;
+	  $summaryfields{$1} = 1;
+	}
+      }
     }
   }
 }
+
+my @summaryfields = sort keys %summaryfields;
 
 my @xy = qw(ac ag at ca cg ct ga gc gt ta tc tg);
 my %m;
 my %n;
 
-print (join ("\t", qw(m n), @xy), "\n");
+print (join ("\t", qw(m n), @xy, @summaryfields), "\n");
 
 foreach (sort keys %sum)
 {
@@ -39,11 +58,59 @@ foreach (sort keys %sum)
   $m{$m} = 1;
   $n{$n} = 1;
 }
+
+my %wantexamples;
+my %examplefiles;
 for my $m (sort { $a <=> $b } keys %m)
 {
   for my $n (sort { $a <=> $b } keys %n)
   {
-    my @N = map { $sum{"$m,$n,$_"} + 0 } @xy;
+    my @N = map { $sum{"$m,$n,$_"} + 0 } (@xy, @summaryfields);
     print (join ("\t", $m, $n, @N), "\n");
+
+    # add # satisfying reads across all xy
+
+    $sum{"$m,$n"} = 0;
+    map { $sum{"$m,$n"} += $sum{"$m,$n,$_"} } @xy;
+    $sum{"$m,$n"} = 1 if $sum{"$m,$n"} == 0;
+
+    # how many examples should we save? max { 1000, total # satisfying reads }
+
+    my $wantexamples_total = $sum{"$m,$n"} > 1000 ? 1000 : $sum{"$m,$n"};
+    map { $wantexamples{"$m,$n,$_"} = int ($wantexamples_total * $sum{"$m,$n,$_"} / $sum{"$m,$n"}) } @xy;
+
+    # open a file to store the examples for this {m,n}
+
+    $examplefiles{"$m,$n"} = new IO::File;
+    open $examplefiles{"$m,$n"}, ">", "$examplesbase-m$m-n$n.txt"
+	or die "create $examplesbase-m$m-n$n.txt: $!";
   }
 }
+
+$m->rewind;
+while (my $s = $m->subdir_next)
+{
+  $s->rewind;
+  while (my ($pos, $size, $filename) = $s->file_next)
+  {
+    last if !defined $pos;
+    $s->seek ($pos);    
+    while (my $dataref = $s->read_until (undef, "\n"))
+    {
+      if ($$dataref =~ /^\#example: m=(\d+) n=(\d+) ([acgt][acgt]) (.*)/s)
+      {
+	my $m = $1;
+	my $n = $2;
+	my $xy = $3;
+	my $s = $4;
+	if ($wantexamples{"$m,$n,$xy"} > 0)
+	{
+	  --$wantexamples{"$m,$n,$xy"};
+	  print { $examplefiles{"$m,$n"} } "$xy $s";
+	}
+      }
+    }
+  }
+}
+
+map { close $examplefiles{$_} or die "close examples{$_}: $!" } keys %examplefiles;
