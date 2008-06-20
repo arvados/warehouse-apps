419184af57252329f1212bcf0f3b9fe225c270fe
Alexander Wait Zaranek <awaitz@post.harvard.edu>
2008-06-20 19:42:32 +0000

    spent a few hours writing a reducecat -> bed parser.   The main problem is that the coordinates returned by megablast, via megatri, for the reverse strand are non-ideal.
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@2110 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- apps/awz/megaparse.pl
+++ apps/awz/megaparse.pl
@@ -7,105 +7,43 @@ use Getopt::Std;
 my %args;
 getopts ("n:m:", \%args); 
 
-my $query; 
-my $chr; 
-my $strand; 
-my $bp_r;
-my $bp_q; 
+#sample input line  
+#example: m=1 n=5 ga gnl|ti|128860174 ana19b09.y1; agg}aag 5 335 (168 361 412 498 503), ; chr7; M; 117488075; 193;
 
-my $pos_r  = "";
-my $pos_q = ""; 
-my $alt = 0;
 
-my $n = $args{'n'};
-my $m = $args{'m'}; 
+while(<>) {
+  my $input_line = $_;
+  my ($first,$runs,$chr,$strand,$chr_pos,$tr_pos,$other) = split (/;/, $_,7); 
+  $first =~ m/ti\|(\d+)/;
+  my $ti = $1;
 
-sub parse_variants() {
-  
-  if ($bp_r && $bp_q) {
-    my $length_r = length ($bp_r);
-    my $length_q = length ($bp_q);
-
-    if ($length_r == $length_q) {
-       
-      my $summary; 
-
-      my $run = 0; 
-      my @run_string = (""); 
-      my @run_length = (0);
-
-      for (my $i = $m; $i < $length_q-$m; $i++) {
-	my $x = substr($bp_r, $i-$m, 1+2*$m);
-	my $y = substr($bp_q, $i-$m, 1+2*$m);
-	
-	if (substr($x,$m,1) ne substr($y, $m, 1)) {
-	  my $edit = $x."}".$y;
-	  
-	  if ($edit eq $run_string[$run]) {
-	    $run_length[$run]++; 	    
-	  }
-	  else {
-	    $run++;
-	    $run_string[$run] = $edit; 
-	    $run_length[$run] = 1; 
-	  }
-	}
-      }
-      for (my $i=1; $i <=$run; $i++) {
-	if ($run_length[$i] >= $n) {
-
-	  $summary .= "$run_string[$i] $run_length[$i], ";
-	}
-      }
-      if ($summary) {
-	print "$query; $summary; $chr; $strand; $pos_r; $pos_q;". 
-	    "$bp_r; $bp_q\n";
-      }
-      $bp_r = ""; 
-      $bp_q = ""; 
-      $pos_r = "";
-      $pos_q = ""; 
-    }
-    else {
-      warn "query and reference mismatch\n***$bp_r***\n***$bp_q***\n";
-    }
-  } 
-}
-
-while (<>) {
-  
-  chomp;
-  
-  my $input  = $_; 
-
-  if ($input =~ m/^Query= (.*)$/) { 
-    parse_variants();
-    $query = $1;
-  }
-  elsif ($input =~ m/^.....: ([0-9]+)\s+(.+) [0-9]+/ ) { 
-    if ($alt) {
-      $alt = 0;
+  #strip off white space 
+  $chr =~ s/ //; 
+  $strand =~ s/ //; 
+  $chr_pos =~ s/ //; 
+  $tr_pos =~ s/ //; 
  
-      if ($pos_r eq "") {
-	$pos_r = $1;
+  my @edits = split (/,/,$runs); 
+  foreach my $edit (@edits) {
+    if ($edit =~ m/(...}...) \d+ (\d+) \((.*)\)/ && $2 >= 100) {
+      my $type = $1; 
+      my @runpos = split (/ /, $3); 
+      foreach my $pos (@runpos) {
+        my $chrpos0;
+	my $chrpos1; 
+        if ($strand =~ m/P/) {
+          $chrpos0= $chr_pos+$pos-2;
+	  $chrpos1= $chr_pos+$pos+1;
+        }
+        elsif ($strand =~ m/M/) {
+          $chrpos0= $chr_pos+$pos-2;
+	  $chrpos1= $chr_pos+$pos+1;
+        }
+        else {
+          die "parse error\n"
+        }
+        print "$chr $chrpos0 $chrpos1 $type|$strand|$chr_pos|$pos|$tr_pos|ti|$ti\n";
       }
-      $bp_r .= $2;
     }
-    else {
-      $alt = 1;  
-
-      if ($pos_q eq "") {
-	$pos_q = $1; 
-      }
-      $bp_q .= $2;
-    } 
   }
-  elsif ($input =~ m/^ Strand = Plus \/ (.)/) {
-    parse_variants();
-    $strand = $1;     
-  }
-  elsif ($input =~ m/^>(.*)/) {
-    $chr = $1; 
-  }
-}  
-
+}
