8e19982673420954644c81098711d06878b38cb8
Tom Clegg <tom@clinicalfuture.com>
2008-12-19 10:44:11 +0000

    Safepipe: fix error message
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@2558 1b2bb62c-e52f-0410-8f12-f050cf38ac25

--- mapreduce/Safepipe.pm
+++ mapreduce/Safepipe.pm
@@ -46,7 +46,7 @@ sub readfrom
 	    }
 
 	    exec $command;
-	    die "@caller: $!";
+	    die "$command: $! at $caller[1] line $caller[2].\n";
 	}
 	close "write$n" or die "$!";
 	$lastreadpipe = "read$n";
