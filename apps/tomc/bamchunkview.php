28d4173b2450c87bc09519e76958353a05af78d6
Tom Clegg <tom@clinicalfuture.com>
2010-02-24 23:55:44 +0000

    add bamchunkview
    git-svn-id: http://dev.oxf.freelogy.org/svn/polony/polony-tools/trunk@3039 1b2bb62c-e52f-0410-8f12-f050cf38ac25

---
+++ apps/tomc/bamchunkview.php
@@ -0,0 +1,9 @@
+<?php
+
+list ($none, $chr, $pos) = explode ('/', $_SERVER["PATH_INFO"]);
+
+print "<PRE>";
+passthru ("/home/tomc/pt/apps/tomc/bamchunkview a2ca1ff415fcbc1c04f52cf472437eb3+564635+K@templeton $chr $pos");
+print "</PRE>";
+
+?>
