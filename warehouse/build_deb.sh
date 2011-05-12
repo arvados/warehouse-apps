4cabba7a0b7ffed91e1705cd3d6fa3c952433142
Tom Clegg <tom@clinicalfuture.com>
2011-05-12 14:43:13 -0700

    fix error checking

--- warehouse/build_deb.sh
+++ warehouse/build_deb.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e -x
 
 # Write changelog
 git log --pretty="libwarehouse-perl (%ct.%h) hardy lucid maverick natty lenny squeeze; urgency=low%n  * %s%n    commit:%H%n -- %an <%ae>  %cD%n" --diff-filter='[A|C|D|M|R|T]' . > debian/changelog
