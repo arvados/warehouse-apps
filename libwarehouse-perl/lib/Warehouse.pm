a6d3e3d42d9b8822931b4992574d2ed7cbc49724
Tom Clegg <tom@clinicalfuture.com>
2011-09-08 17:13:42 -0400

    add curl request timeout

--- libwarehouse-perl/lib/Warehouse.pm
+++ libwarehouse-perl/lib/Warehouse.pm
@@ -1219,7 +1219,7 @@ sub fetch_from_keep
 	my ($status_number, $status_phrase);
 	my $data;
 	if ($Warehouse::HTTP::useCurlCmd &&
-	    open F, '-|', 'curl', '--fail', '-s', $url) {
+	    open F, '-|', 'curl', '--max-time', $self->{timeout}, '--fail', '-s', $url) {
 	    $data = "";
 	    my $bytes = 0;
 	    do {
