f466269532ac52b9a4df90a062b144979354da85
Tom Clegg <tom@clinicalfuture.com>
2011-11-09 22:55:11 -0500

    add ::wh_stream_segment, support submodules in ::git, fix races and unnecessary work in ::wh_file

--- mapreduce/Safeget.pm
+++ mapreduce/Safeget.pm
@@ -35,23 +35,58 @@ sub wh_file
     my ($data_locator, $local_file) = @_;
     $local_file
 	or die "no local_file specified";
-    return 1 if (readlink ("$local_file.locator") eq md5_hex($data_locator) &&
-		 -e $local_file);
-    if (open (L, "+>>", "$local_file.lock") &&
-	flock (L, LOCK_EX) )
+    unless (open (L, "+>>", "$local_file.lock") &&
+	    flock (L, LOCK_EX))
     {
-	if (0 != system "whget '$data_locator' '$local_file.tmp'")
-	{
-	    unlink "$local_file.tmp";
-	    close L;
-	    die "whget exited $?";
-	}
-	symlink md5_hex($data_locator), "$local_file.locator";
-	rename "$local_file.tmp", $local_file
-	    or die "rename $local_file.tmp failed: $!";
+	die "Failed to lock $local_file.lock";
     }
+    if (readlink ("$local_file.locator") eq md5_hex($data_locator) &&
+	-e $local_file) {
+	close L;
+	return 1;
+    }
+    if (0 != system "whget '$data_locator' '$local_file.tmp'")
+    {
+	unlink "$local_file.tmp";
+	close L;
+	die "whget exited $?";
+    }
+    rename "$local_file.tmp", $local_file
+	or die "rename $local_file.tmp failed: $!";
+    symlink md5_hex($data_locator), "$local_file.locator";
     close L;
-    die "Failed to whget $data_locator -> $local_file" if !-e $local_file;
+    return 1;
+}
+
+sub wh_stream_segment
+{
+    my ($stream, $pos, $size, $local_file) = @_;
+    $local_file
+	or die "no local_file specified";
+
+    my $id = md5_hex($stream->as_string . " $pos $size");
+
+    unless (open (L, "+>>", "$local_file.lock") &&
+	    flock (L, LOCK_EX) )
+    {
+	die "Failed to lock $local_file.lock";
+    }
+
+    if (readlink ("$local_file.locator") eq $id &&
+	-e $local_file) {
+	close L;
+	return 1;
+    }
+    open F, ">", "$local_file.tmp" or die "open $local_file.tmp: $!";
+    $stream->seek($pos);
+    while (my $dataref = $stream->read_until ($pos + $size)) {
+	print F $$dataref or die "write $local_file.tmp: $!";
+    }
+    close F or die "close $local_file.tmp: $!";
+    rename "$local_file.tmp", $local_file
+	or die "rename $local_file.tmp failed: $!";
+    symlink $id, "$local_file.locator";
+    close L;
     return 1;
 }
 
@@ -59,7 +94,12 @@ sub git {
     my ($git_url, $local_dir, $checkout_tag) = @_;
     $local_dir
 	or die "no local_dir specified";
-    my $sourcetag = md5_hex($git_url.$;.$checkout_tag);
+
+    my $commit = `git ls-remote '$git_url' '$checkout_tag' | head -c40`;
+    die "Failed to find commit-ish $checkout_tag at $git_url"
+	unless $commit =~ /^[0-9a-f]{40}$/;
+
+    my $sourcetag = md5_hex($git_url.$;.$commit);
     my ($local_dir_basename) = $local_dir =~ /([^\/]+)$/;
     return 1 if (readlink ($local_dir) eq "$local_dir_basename.$sourcetag" &&
 		 -d $local_dir);
@@ -69,11 +109,14 @@ sub git {
 	system ("rm", "-rf", "$local_dir.$sourcetag");
 	system ('git', 'clone', $git_url, "$local_dir.$sourcetag") == 0
 	    or die "git clone $git_url failed: $?";
-	system ("cd '$local_dir.$sourcetag' && git checkout --quiet '$checkout_tag'") == 0
-	    or do {
+	my @cmds = ("git checkout --quiet '$commit'",
+		    "[ ! -e .gitmodules ] || ( git submodule -q init && git submodule -q sync && git submodule -q update --init --recursive )");
+	for (@cmds) {
+	    unless (system ("cd '$local_dir.$sourcetag' && ( $_ )") == 0) {
 		system ("rm", "-rf", "$local_dir.$sourcetag");
-		die "git checkout --quiet $checkout_tag failed: $?";
-	};
+		die "$_ failed: $?";
+	    }
+	}
 	unlink "$local_dir.tmp";
 	symlink "$local_dir_basename.$sourcetag", "$local_dir.tmp"
 	    or die "symlink $local_dir.tmp -> $local_dir_basename.$sourcetag failed: $!";
