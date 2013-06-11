c34566454aac76efbadb5818352bfa682ef03b5a
Ward Vandewege <ward@jhvc.com>
2013-06-10 20:45:20 -0400

    Also build packages for wheezy and precise (part 2).

--- libwarehouse-perl/build_deb.sh
+++ libwarehouse-perl/build_deb.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 # Write changelog
-git log --pretty=format:"libwarehouse-perl (%ct.%h) hardy lucid maverick natty lenny squeeze; urgency=low%n  * %s%n    commit:%H%n -- %an <%ae>  %cD%n" . > debian/changelog
+git log --pretty=format:"libwarehouse-perl (%ct.%h) hardy lucid maverick natty precise lenny squeeze wheezy; urgency=low%n  * %s%n    commit:%H%n -- %an <%ae>  %cD%n" . > debian/changelog
 
 perl Makefile.PL INSTALLDIRS=vendor
 dpkg-buildpackage -rfakeroot -uc -us
